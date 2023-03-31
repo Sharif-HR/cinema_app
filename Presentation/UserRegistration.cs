62856cefa1968b4512fc1e5cc57afe032c3c2087
Jurian-24 <jurian.andriessen@gmail.com>
2023-03-31 11:41:59 +0200

    chore(feat): changed the input fields

--- Presentation/UserRegistration.cs
+++ Presentation/UserRegistration.cs
@@ -8,13 +8,13 @@ static class UserRegistration
         Console.WriteLine("Welcome to the registration page.");
         string id = accountsLogic.GenerateUUID();
 
-        string userName = Inputs.InputField("Enter your username");
-        string firstName = Inputs.InputField("Enter your first name:");
-        string lastName = Inputs.InputField("Enter your last name:");
-        string email = Inputs.InputField("Enter your email:");
-        string phoneNumber = Inputs.OptionalInput("Enter your phonenumber (optional):");
-        string password = Inputs.PasswordInput();
-        string isEligible = Inputs.InputField("Eligible (Y/n):", true, new List<string>(){"y", "n", "Y", "N"});
+        string? userName = Inputs.InputField("Enter your username");
+        string? firstName = Inputs.InputField("Enter your first name:");
+        string? lastName = Inputs.InputField("Enter your last name:");
+        string? email = Inputs.InputField("Enter your email:");
+        string? phoneNumber = Inputs.OptionalInput("Enter your phonenumber (optional):");
+        string? password = Inputs.PasswordInput();
+        string? isEligible = Inputs.InputField("Eligible (Y/n):", true, new List<string>(){"y", "n", "Y", "N"});
 
         AccountModel acc = new AccountModel(id, userName, password, email, phoneNumber, firstName, lastName, isEligible, "Customer");
         accountsLogic.UpdateList(acc);
