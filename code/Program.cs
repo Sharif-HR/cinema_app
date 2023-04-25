6e0d4fc834996fac15d5c22f8e73c72219306090
Jurian-24 <jurian.andriessen@gmail.com>
2023-04-25 16:11:55 +0200

    chore(build): Added the ENV file functionality

--- code/Program.cs
+++ code/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
 
 namespace CinemaApp
 {
@@ -6,6 +9,13 @@ namespace CinemaApp
     {
         static void Main(string[] args)
         {
+            var root = Directory.GetCurrentDirectory();
+            var dotenv = Path.Combine(root, ".env");
+
+            DotEnv.Load(dotenv);
+
+            Console.Write(Environment.GetEnvironmentVariable("APP_NAME"));
+            return;
             Console.Clear();
             Views.Menu MenuPage = new();
             MenuPage.Render();
