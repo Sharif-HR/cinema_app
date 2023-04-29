1c8e378900562c42213ca22b4829176f721fe65c
Jurian-24 <jurian.andriessen@gmail.com>
2023-04-29 22:56:00 +0200

    chore(feat): added localstorage into the application

---
+++ code/Logic/LocalStorage.cs
@@ -0,0 +1,120 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Reflection;
+
+public static class LocalStorage {
+    // Create the storage
+    public static Dictionary<string, dynamic> localStorage = new();
+
+    public static void setItem(string key, object value) => localStorage.Add(key, value);
+
+
+    public static object? GetItem(string key) => LocalStorageKeyCheck(key) ? localStorage[key] : null;
+
+    public static void WriteToStorage() {
+        AccountModel? user = GetAuthenticatedUser();
+        JsonNode mainJson = JsonNode.Parse("{}");
+
+        // goed
+        mainJson["authenticated"] = true;
+
+        JsonObject userJson = new();
+
+        // create a list of the properties of the AccountModel class
+        PropertyInfo[] properties = user.GetType().GetProperties();
+
+        // loop through the properties
+        foreach (PropertyInfo property in properties) {
+            // assign the property name as the json key with the value of the property as its value. Example: "firstName": "test"
+            userJson[Helpers.DecapitalizeString(property.Name)] = JsonNode.Parse(JsonSerializer.Serialize(property.GetValue(user)));
+        }
+
+        mainJson["user"] = userJson;
+        JsonArray historyArray = new JsonArray();
+
+        foreach(var item in localStorage["history"]) {
+            historyArray.Add(item);
+        }
+
+        mainJson["history"] = historyArray;
+
+        var options = new JsonSerializerOptions {
+            WriteIndented = true
+        };
+
+        var jsonString = JsonSerializer.Serialize(mainJson, options);
+
+        using (StreamWriter sw = File.CreateText("data/localStorage.json")){ sw.WriteLine(jsonString); }
+    }
+
+    public static void LoadLocalStorage() {
+        var json = File.ReadAllText("data/localStorage.json");
+        var data = JsonSerializer.Deserialize<Dictionary<string, object>>(json);
+        foreach (var item in data)
+        {
+            if(item.Key == "user") {
+                localStorage[item.Key] = JsonSerializer.Deserialize<AccountModel>(item.Value.ToString());
+            } else if (item.Key == "history") {
+                JsonElement historyElement = (JsonElement)data["history"];
+                List<string> historyList = new List<string>();
+
+                if(historyElement.ValueKind == JsonValueKind.Array) {
+                    foreach (JsonElement element in historyElement.EnumerateArray())
+                    {
+
+                        if(element.ValueKind == JsonValueKind.String) {
+                            historyList.Add(element.GetString());
+                        }
+                    }
+                    localStorage["history"] = historyList;
+                }
+            }
+            else {
+                localStorage[item.Key] = item.Value;
+            }
+        }
+    }
+
+    private static bool LocalStorageKeyCheck(string key) {
+        return (localStorage.ContainsKey(key) && localStorage[key] != null);
+    }
+
+    public static AccountModel? GetAuthenticatedUser() {
+        AccountModel? user = null;
+        if(LocalStorageKeyCheck("user")) {
+            user = (AccountModel)localStorage["user"];
+        }
+
+        return user;
+    }
+
+    public static bool IsAuthenticated() {
+        if(LocalStorageKeyCheck("authenticated")) {
+            return (bool)localStorage["authenticated"];
+        }
+
+        return false;
+    }
+
+    public static void ClearStorage() {
+        List<string> data = new(){};
+        File.WriteAllLines("data/localStorage.json", data);
+    }
+
+    public static void AddToHistory(string pageTitle) {
+        if(LocalStorageKeyCheck("history")) {
+            localStorage["history"].Add(pageTitle);
+            WriteToStorage();
+        }
+    }
+
+    public static string? LastVisitedPage() {
+        if(LocalStorageKeyCheck("history")) {
+            return localStorage["history"][localStorage["history"].Count - 1];
+        }
+
+        return null;
+    }
+}
