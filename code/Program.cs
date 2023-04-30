99696494ed5d68a65b7e7e71d75a73f175bdeb77
Jurian-24 <jurian.andriessen@gmail.com>
2023-04-30 22:47:29 +0200

    chore(feat): Added the localstorage to the core of the program

--- code/Program.cs
+++ code/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Threading;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.Json;
 
@@ -12,11 +15,26 @@ namespace CinemaApp
             var root = Directory.GetCurrentDirectory();
             var dotenv = Path.Combine(root, ".env");
 
+            // Load the env file
             DotEnv.Load(dotenv);
+            // Load the local storage
+            LocalStorage.LoadLocalStorage();
 
-            Console.Clear();
-            Views.Menu MenuPage = new();
-            MenuPage.Render();
+            // start the go back function so that it is always listening for input
+            Thread backgroundThread = new Thread(new ThreadStart(GoBack));
+            backgroundThread.Start();
+
+            Views.RouteHandeler.View("MenuPage");
+        }
+
+        static void GoBack() {
+            ConsoleKeyInfo Key = Console.ReadKey(true);
+
+            if(Key.Key == ConsoleKey.Escape) {
+                Console.WriteLine("Directing you back...");
+                Thread.Sleep(5000);
+                Views.RouteHandeler.LastView();
+            }
         }
     }
 }
