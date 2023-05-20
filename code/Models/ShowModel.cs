266ae9d57f7daa4776daf1e548ece816903fef7a
Jurian-24 <jurian.andriessen@gmail.com>
2023-05-20 17:18:41 +0200

    chore(build): added the takenseats field to the showmodel

--- code/Models/ShowModel.cs
+++ code/Models/ShowModel.cs
@@ -6,15 +6,20 @@ public class ShowModel {
 
     [JsonPropertyName("timestamp")]
     public int Timestamp {get; set;} // timestamp
+    [JsonPropertyName("takenSeats")]
+    public List<string> TakenSeats {get; set;}
+
     [JsonPropertyName("numberOfSeats")]
     public int NumberOfSeats {get; set;}
+
     [JsonPropertyName("movie")]
     public MovieModel Movie {get; set;}
 
-    public ShowModel(string showId, int timestamp, int numberOfSeats, MovieModel movie) {
+    public ShowModel(string showId, int timestamp, int numberOfSeats, List<string> takenSeats, MovieModel movie) {
         this.showId = showId;
         this.Timestamp = timestamp;
         this.NumberOfSeats = numberOfSeats;
+        this.TakenSeats = takenSeats;
         this.Movie = movie;
     }
 }
