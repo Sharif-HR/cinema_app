d61b5a35e3f8d9d59ebcd1d1d2f2b9afa3771db2
sharif <sharif.choudry@gmail.com>
2023-06-07 08:06:01 +0200

    added new unitTest for example

--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -5,6 +5,36 @@ public class UnitTest1
 
 
     [Fact]
+    public void ExampleTest()
+    {
+        // Your function here
+        List<string> fruits = new List<string>(){
+            "apple", "banana", "orange", "berries"
+        };
+
+        fruits.Remove("banana");
+
+        bool hasBanana = fruits.Contains("banana");
+
+        // this is a scenario where we excpect that the result is true
+        Assert.Equal(false, hasBanana);
+    }
+
+    [Fact]
+    public void TestAddMovie(){
+        MovieLogic movieLogic = new MovieLogic(_currentPath);
+        MovieModel movie = new MovieModel("test", 120, "test movie description", new List<string>{"Horror"}, "Test Dir", "07-06-2015", "08-06-2023");
+        string movieId = movie.Id;
+
+        movieLogic.AddMovie(movie);
+
+        bool foundMovie = movieLogic.GetMovies().Any(m => m.Id == movieId);
+
+        Assert.Equal(true, foundMovie);
+    }
+
+
+    [Fact]
     public void TestLoginCorrectCredentials()
     {
         bool loginSuccess = false;
